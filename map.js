698e18aa20e101eb07e00d7f1ee5e8d33a006b14
PM <121531128+26-PM@users.noreply.github.com>
2023-12-18 19:01:31 +0530

    Create map.js

---
+++ map.js
@@ -0,0 +1,36 @@
+// Reduce Map Filter
+
+array=[1,2,3,4,5,6,7,8,9,10]
+
+
+
+Map
+
+console.log(array.map((e)=>{
+    return e**2;
+})
+)
+
+
+
+
+// Filter
+
+// function greaterThen5(a){
+//     if (a>5){
+//         return true;
+//     }
+//     return false;
+// }
+// console.log(array.filter(greaterThen5));
+
+
+
+// Reduce
+
+// const sum=(a,b)=>{
+//     return a+b;
+// }
+// console.log(array.reduce(sum))
+
+
